696510dd20fc74691125962d435be22fedc36bd4
Samantha Bales <samanthabales2006@gmail.com>
2025-11-16 12:06:51 -0500

    Changed HTML links from absolute to relative paths for compatibility on other devices and/or test servers

--- includes/head.php
+++ includes/head.php
@@ -2,4 +2,4 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?php print $page_title; ?></title>
 <meta name="description" content=<?php print $page_description; ?>>
-<link rel="stylesheet" href="/style.css">
+<link rel="stylesheet" href="style.css">
